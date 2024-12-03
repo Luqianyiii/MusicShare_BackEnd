c7ea4dc295e3019276bee21b3800eadca3d05ff5
小何先生 <3031933702@qq.com>
2024-12-03 08:45:49 +0800

    提交

---
+++ src/test/java/com/hahaha/musicshare/MusicShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hahaha.musicshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
