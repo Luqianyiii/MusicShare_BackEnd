62036af34688e99ee0bfa99e8b98298784b5ae03
小何先生 <3031933702@qq.com>
2024-12-05 10:49:30 +0800

    修复版本控制失误，导致头像上传到阿里云部分代码丢失问题

--- src/main/java/com/hahaha/musicshare/controller/CommonController.java
+++ src/main/java/com/hahaha/musicshare/controller/CommonController.java
@@ -5,10 +5,8 @@ import com.hahaha.musicshare.service.CommonService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Tag(name = "基础服务")
 @RestController
@@ -21,5 +19,11 @@ public class CommonController {
     public Result<Object> sendSms(@RequestParam("phone") String phone) {
         commonService.sendSms(phone);
         return Result.ok();
+    }
+
+    @PostMapping(value = "/upload/img")
+    @Operation(summary = "图⽚上传")
+    public Result<String> upload(@RequestBody MultipartFile file) {
+        return Result.ok(commonService.upload(file));
     }
 }
