9f83ae88143c5efff7be266c4a7a4064423503d2
小何先生 <3031933702@qq.com>
2024-12-11 09:11:23 +0800

    Revert "添加数据加载层，需要设计，Model层待完善"
    This reverts commit f95f2d8dc37ea21b6d7e23c5b27f886dbd423664.

--- src/main/java/com/hahaha/musicshare/service/LoadService.java
+++ src/main/java/com/hahaha/musicshare/service/LoadService.java
@@ -2,9 +2,8 @@ package com.hahaha.musicshare.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.hahaha.musicshare.model.entity.User;
-import com.hahaha.musicshare.model.vo.UserInfoVO;
 
 public interface LoadService extends IService  {
 
-    UserInfoVO loadUser(int userId);
+    User loadUser(String userId);
 }
